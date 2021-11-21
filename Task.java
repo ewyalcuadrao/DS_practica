5d7ef6df94456e86c1367e25901a6cd29a929faf
borjaarcos <32301583+borjaarcos@users.noreply.github.com>
2021-11-21 11:49:53 +0100

    Update Task.java

--- Task.java
+++ Task.java
@@ -1,19 +1,18 @@
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-public class Task extends Item{
 
-  private ArrayList <Interval> interval;
+public class Task extends Item {
 
-  public Task(String name, Project father)
-  {
+  private ArrayList<Interval> interval;
+
+  public Task(String name, Project father) {
     super(name, father);
     this.interval = new ArrayList<Interval>();
     this.father.addItem(this);
   }
 
-  protected void startWorking()
-  {
+  protected void startWorking() {
     if (!active) {
       Interval i = new Interval(this);
       active = true;
@@ -21,57 +20,58 @@ public class Task extends Item{
       interval.add(i);
       System.out.println(this.name + " starts");
     }
-   }
-  public void stopWorking()
-  {
+  }
+
+  public void stopWorking() {
     active = false;
     father.setActive(false);
-    interval.get(interval.size()-1).stopInterval();
-    this.updateTotalTime(interval.get(interval.size()-1).getDuration());
+    interval.get(interval.size() - 1).stopInterval();
+    this.updateTotalTime(interval.get(interval.size() - 1).getDuration());
     System.out.println(this.name + " stops");
   }
 
   @Override
-  protected Duration getDurationBetween(LocalDateTime ini, LocalDateTime end)
-  {
+  protected Duration getDurationBetween(LocalDateTime ini, LocalDateTime end) {
     Duration duration = Duration.ZERO;
     boolean cond = false;
-    for (int i = 0; i < interval.size(); i++){
-      cond = (interval.get(i).getInitTime().isAfter(ini) || interval.get(i).getInitTime().isEqual(ini)) &&
-          (interval.get(i).getEndTime().isBefore(end) || interval.get(i).getEndTime().isEqual(end));
+    for (int i = 0; i < interval.size(); i++) {
+      cond = (interval.get(i).getInitTime().isAfter(ini)
+          || interval.get(i).getInitTime().isEqual(ini))
+          && (interval.get(i).getEndTime().isBefore(end)
+          || interval.get(i).getEndTime().isEqual(end));
 
-      if (cond)
-          duration = duration.plus(interval.get(i).getDuration());
+      if (cond) {
+        duration = duration.plus(interval.get(i).getDuration());
+      }
     }
     return duration;
   }
 
   @Override
-  public Duration getTotalTime()
-  {
-    if(!active)
+  public Duration getTotalTime() {
+    if (!active) {
       return this.totalTime;
-    else
-    {
-      Duration d = interval.get(interval.size()-1).getDuration();
+    } else {
+      Duration d = interval.get(interval.size() - 1).getDuration();
       return this.totalTime.plus(d);
-     }
+    }
   }
 
   @Override
-  public void acceptVisitor(Visitor v){
+  public void acceptVisitor(Visitor v) {
     v.visitTask(this);
   }
 
 
-  public Interval getLastInterval(){
-    return interval.get(interval.size()-1);
+  public Interval getLastInterval() {
+    return interval.get(interval.size() - 1);
   }
-  public ArrayList<Interval> getIntervals(){
+
+  public ArrayList<Interval> getIntervals() {
     return interval;
   }
 
-  public void addInterval(Interval i){
+  public void addInterval(Interval i) {
     interval.add(i);
   }
 }
