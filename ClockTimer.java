5628d6cc7ed4233dd7fb373537926b5798c90c4e
borjaarcos <32301583+borjaarcos@users.noreply.github.com>
2021-11-21 11:44:22 +0100

    Update ClockTimer.java

--- ClockTimer.java
+++ ClockTimer.java
@@ -1,16 +1,22 @@
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.Observable;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class ClockTimer extends Observable{
+public class ClockTimer extends Observable {
   private Timer timer;
   private LocalDateTime dateTime;
-  private static ClockTimer instance= null;
+  private static ClockTimer instance = null;
   private static final int periode = 2; //seconds
 
-  private ClockTimer() {this.start();}
+  private ClockTimer() {
+    this.start();
+  }
 
-  public static ClockTimer getInstance(){
-    if(instance == null){instance = new ClockTimer();}
+  public static ClockTimer getInstance() {
+    if (instance == null) {
+      instance = new ClockTimer();
+    }
     return instance;
   }
 
@@ -26,10 +32,15 @@ public class ClockTimer extends Observable{
     timer.scheduleAtFixedRate(repeatedTask, 0, 1000 * periode);
   }
 
-  public void stop(){
+  public void stop() {
     timer.cancel();
   }
-  public LocalDateTime getDate(){return dateTime;}
 
-  public static int getPeriode() { return periode; }
+  public LocalDateTime getDate() {
+    return dateTime;
+  }
+
+  public static int getPeriode() {
+    return periode;
+  }
 }
